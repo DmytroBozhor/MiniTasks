31a0cc911782cab21e70f2aa6b58fd02a687a19b
DmytroBozhor <dimaastop1@gmail.com>
2023-09-19 19:08:05 +0200

    spring project init

---
+++ src/test/java/com/example/practicejunior/PracticeJuniorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.practicejunior;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeJuniorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
