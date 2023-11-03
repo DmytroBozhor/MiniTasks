fce8bc94c7e57240025428808c0b2f2baad09fe4
DmytroBozhor <dimaastop1@gmail.com>
2023-11-03 16:40:45 +0100

    immutable class example

---
+++ src/main/java/com/example/practicejunior/task2/Money.java
@@ -0,0 +1,13 @@
+package com.example.practicejunior.task2;
+
+public final class Money {
+    private final int value;
+
+    public Money(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
